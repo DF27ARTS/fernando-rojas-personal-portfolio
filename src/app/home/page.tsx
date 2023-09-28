b6c70a900795d0296e877cd17e30074f6949e804
Fernando Rojas Carrillo <98248506+DF27ARTS@users.noreply.github.com>
2023-09-28 00:16:28 +0000

    header text animation in progress

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,19 @@
+import FullName from "@/components/FullName";
+
 const HomePage = () => {
+    
     return (
         <div className="home-page" >
-            Hello
+            <section className="home-header">
+                <div>
+                    <div className="profile-picture"></div>
+                </div>
+                <div>
+                    <div className="profile-title">
+                        <FullName />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
---
+++ src/components/FullName.tsx
@@ -0,0 +1,26 @@
+const FullName = () => {
+    const NAME = "FERNANDO ROJAS"
+    return (
+        <div className="profile-name-container">
+            {NAME.split(" ").map((word, index) => {
+                const wordDelay = index !== 0 ? NAME.split(" ")[0].length * 100 : 0;
+                return (
+                    <div 
+                    key={index}
+                    style={{"--word-delay": `${wordDelay + 1}ms`} as React.CSSProperties}
+                    className="single-word">
+                        {word.split("").map((letter, index) => {
+                            return (
+                                <div key={index} className="single-letter">
+                                    {letter}
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default FullName;
