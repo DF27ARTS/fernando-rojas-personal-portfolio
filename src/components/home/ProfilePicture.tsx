cc6e8dbd2c736bae27a17b4a8a4b203d01534ab5
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2023-12-16 16:27:36 -0500

    Add: the profile picture background animation

--- src/components/home/ProfilePicture.tsx
+++ src/components/home/ProfilePicture.tsx
@@ -1,12 +1,19 @@
 import picture from "@/assets/profile-picture-2.png";
+import Image from "next/image";
 
 const ProfilePicture = () => {
-    return (
-        <div className="profile-picture">
-            <div className="gradient-background"></div>
-            <img src={picture.src} className="picture" />
-        </div>
-    )
-}
+  return (
+    <div className="profile-picture">
+      <div className="gradient-background"></div>
+      <Image
+        height="1200"
+        width="1200"
+        src={picture.src}
+        alt="Portfolio picture"
+        className="picture"
+      />
+    </div>
+  );
+};
 
 export default ProfilePicture;
